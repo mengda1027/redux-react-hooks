8fc6199fb8644f7afe813e2362310a06f161ca80
Meta-D <mengda1027@gmail.com>
2021-05-30 10:42:45 +0800

    06.Split your Redux Store into Multiple Reducers

---
+++ src/reducers/UserReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+  userName: "meng",
+  logged: false
+};
+
+export function userReducer(state = initialState, action) {
+  return state;
+}
--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import { rateReducer } from "./reducers/RateReducer";
+import { userReducer } from "./reducers/UserReducer";
 
 const rootReducer = combineReducers({
-  rate: rateReducer
+  rate: rateReducer,
+  user: userReducer
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
